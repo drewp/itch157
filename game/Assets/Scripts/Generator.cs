428154ce97356f041f42363f6d4a6002c363e8f8
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-25 22:26:27 -0700

    bAT PREFA

--- game/Assets/Scripts/Generator.cs
+++ game/Assets/Scripts/Generator.cs
@@ -6,6 +6,8 @@ public class Generator : MonoBehaviour
 {
     public float Power = 1000;
     public float MaxPower = 1000;
+    float ToRecharge = 0;
+    float Decay = 0;
     void Start()
     {
         
@@ -14,10 +16,29 @@ public class Generator : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Power += ToRecharge;
+        ToRecharge -= Decay;
+        if(ToRecharge <= 0)
+        {
+            Decay = 0;
+            ToRecharge = 0;
+        }
         Power -= 1f;
         if(Power <= 0)
         {
             Power = 0;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Battery")
+        {
+            Debug.Log(collision.gameObject.tag);
+            ToRecharge = collision.gameObject.GetComponent<BatteryScriptable>().PowerRefilled;
+            Decay = collision.gameObject.GetComponent<BatteryScriptable>().TimeToRefill;
+            Destroy(collision.gameObject);
         }
     }
 }
