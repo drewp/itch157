220525ddcc6151750e1f89adef727cfd28b11bb5
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-25 22:41:14 -0700

    vcollsion dont wokerwe

--- game/Assets/Scripts/Generator.cs
+++ game/Assets/Scripts/Generator.cs
@@ -30,7 +30,7 @@ public class Generator : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collision2D collision)
     {
         Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.tag == "Battery")
