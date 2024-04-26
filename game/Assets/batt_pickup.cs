06ec8737dca477389f743480167f2452769208b8
arrowira <41136800+arrowira@users.noreply.github.com>
2024-04-25 23:01:27 -0700

    bat pickup (better)
    press E to pick up.

--- game/Assets/batt_pickup.cs
+++ game/Assets/batt_pickup.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.U2D;
 
 public class batt_pickup : MonoBehaviour
 {
+    private GameObject batt;
+    
+    private bool inRange = false;
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "battery")
         {
-            Destroy(col.gameObject);
+            inRange = true;
+            batt = col.gameObject;
         }
         
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "battery")
+        {
+            inRange = true;
+        }
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inRange == true)
+        {
+            Destroy(batt);
+        }
     }
 }
