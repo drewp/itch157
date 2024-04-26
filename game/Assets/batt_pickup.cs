0a0e48af44f2081db7bcd6755438ef9992c96ef9
arrowira <41136800+arrowira@users.noreply.github.com>
2024-04-25 23:56:16 -0700

    batt pick up and through
    press e for interactions w/ battery

--- game/Assets/batt_pickup.cs
+++ game/Assets/batt_pickup.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.U2D;
+using UnityEngine.XR;
 
 public class batt_pickup : MonoBehaviour
 {
     private GameObject batt;
-    
+    [SerializeField]
+    private GameObject heldbatt;
     private bool inRange = false;
+    private bool holding = false;
+    private bool facingright = true;
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.tag == "Battery")
+        if(col.tag == "Battery" && holding == false)
         {
             inRange = true;
             batt = col.gameObject;
@@ -22,15 +26,49 @@ public class batt_pickup : MonoBehaviour
     {
         if (col.tag == "Battery")
         {
-            inRange = true;
+            inRange = false;
         }
 
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && inRange == true)
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            Destroy(batt);
+            facingright= true;
         }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            facingright= false;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log(holding);
+            if (holding == false)
+            {
+                if (inRange == true)
+                {
+                    holding = true;
+                    Destroy(batt);
+
+                }
+            }
+            else
+            {
+
+                var instance = Instantiate(heldbatt, transform.position, Quaternion.identity);
+                if (facingright == true)
+                {
+                    instance.GetComponent<Rigidbody2D>().AddForce(transform.right * 100);
+                }
+                else
+                {
+                    instance.GetComponent<Rigidbody2D>().AddForce(transform.right * -100);
+                }
+                
+
+                holding = false;
+            }
+            
+        }
+
     }
 }
