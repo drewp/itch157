23b5a0e122da7b41e90ee4b61be8adff64e9567c
arrowira <41136800+arrowira@users.noreply.github.com>
2024-04-26 00:23:36 -0700

    walking animations
    pretty basic walking animations. Hope to add dodge roll and battery pick ups tomorrow

--- game/Assets/Scripts/player_animations.cs
+++ game/Assets/Scripts/player_animations.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class player_animations : MonoBehaviour
 {
+    private bool facingright = true;
+    [SerializeField]
+    private SpriteRenderer sr;
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Animator anm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anm = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.x != 0 || rb.velocity.y != 0)
+        {
+            anm.SetBool("isWalking", true);
+            anm.SetBool("isIdle", false);
+
+        }
+        else if (rb.velocity.x == 0 && rb.velocity.y == 0)
+        {
+            anm.SetBool("isWalking", false);
+            anm.SetBool("isIdle", true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            facingright = true;
+            sr.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            facingright = false;
+            sr.flipX = true;
+        }
     }
 }
