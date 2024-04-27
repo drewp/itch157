bbf9f677ce3b8ef13e3780a9ce8ed2c14883d342
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-27 09:38:04 -0700

    Powerup Variables Added

---
+++ game/Assets/Scripts/PowerUpVariables.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpVariables : MonoBehaviour
+{
+    public float EnergyDrainMod; // Speed Of Light -1
+    public float ClockSpeedMod; // Speed Of Light +1
+    public float AttackSpeedMod; // Degreased Fingers
+    public float HealthAddMod; // Steel Gears
+    public float BatteryDropChanceMod; // Delicate Fingers
+    public float BatteryRechargeMod; // Bigger Batteries
+    public float ClockPowerAddMod; // Unlimited Power
+    public float DamageMod; // Steroids
+    public float MovementSpeedMod; // Fast Feet
+
+    public float PushBatteryMod; // Big Pecs
+    public float BatteryCarrySpeedMod; // Creatine
+    public float BatteryThrowingDistanceAddMod; // Thor
+    public float EnemyExplosionChance; // Greasy Gears
+    public float SubTotalDamageTakenMod; // Titanium Gears
+    public float ClockPowerMod; // Clock Power
+    public float ImmunityFramesAddMod; // Ninja
+
+    public float BatteryExplosionChance; // Chemical Warfare
+    public float BatteryHitMoveMod; // Baseball
+    public float FullyRechargeChanceMod; // Cosmic Rays
+
+    public bool MechanicsGambit;
+    public bool ImHungry;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
