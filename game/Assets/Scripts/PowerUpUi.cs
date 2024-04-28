d78174ecdeb230170706cb02b8989813f55ff48f
PopcornCaleb <calebcukierman@gmail.com>
2024-04-28 01:16:33 -0700

    becase

--- game/Assets/Scripts/PowerUpUi.cs
+++ game/Assets/Scripts/PowerUpUi.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class PowerUpUi : MonoBehaviour
 {
-    public List<Sprite> PowerUpSprites;
-    
+    private List<Sprite> PowerUpSprites;
+    private List<GameObject> PowerUpHolders = new List<GameObject>();
+    void Start()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            PowerUpHolders.Add(gameObject.transform.GetChild(i).gameObject); 
+        }
+        for (int i = 0;i < PowerUpHolders.Count; i++)
+        {
+            PowerUpHolders[i].SetActive(false);
+        }
+            
+    }
+    public void PickedUpPowerUp(Sprite sprite)
+    {
+        if (PowerUpSprites.Find(sprite)!=null)
+        {
+
+        }
+    } 
 }
