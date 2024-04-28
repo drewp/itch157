662255303225970e3c37b682bf703474856705a1
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-28 15:35:03 -0700

    Added Achievment Popups

--- game/Assets/Scripts/PowerUpManager.cs
+++ game/Assets/Scripts/PowerUpManager.cs
@@ -25,68 +25,82 @@ public class PowerUpManager : MonoBehaviour
             Color color = new Color();
             PowerUpVars = GameObject.Find("PowerUpObject");
             PowerPickupSound.GetComponent<AudioSource>().Play();
+            PowerUpUi PUI = GameObject.Find("PowerUpInvintory").GetComponent<PowerUpUi>();
             switch (Type)
             {
                 case 1:
                     //Fast Feet
                     PowerUpVars.GetComponent<PowerUpVariables>().MovementSpeedMod += 0.07f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Fast Feet", "Raises your speed by 7%", color);
                     break;
                 case 2:
                     PowerUpVars.GetComponent<PowerUpVariables>().ClockSpeedMod += 0.10f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Speed Of Light + 1", "Raises clock speed by 10%", color);
                     break;
                 case 3:
                     PowerUpVars.GetComponent<PowerUpVariables>().EnergyDrainMod += 0.10f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Speed Of Light - 1", "Lowers energy drain by 10%", color);
                     break;
                 case 4:
                     PowerUpVars.GetComponent<PowerUpVariables>().ClockPowerAddMod += 0.10f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Unlimited Power", "Adds 100 to the clock power", color);
                     break;
                 case 5:
                     PowerUpVars.GetComponent<PowerUpVariables>().AttackSpeedMod += 0.10f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Degreased Fingers", "Raises attack speed by 10%", color);
                     break;
                 case 6:
                     PowerUpVars.GetComponent<PowerUpVariables>().HealthAddMod += 0.10f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Steel Gears", "Raises mechanics health by 10%", color);
                     break;                
                 case 7:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryDropChanceMod += 20f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Delicate Fingers", "Raises Battery Drop Chance By 10%", color);
                     break;
                 case 8:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryRechargeMod += 0.2f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Bigger Batteries", "Increases amount recharged by batteries by 20%", color);
                     break;
                 case 9:
                     PowerUpVars.GetComponent<PowerUpVariables>().DamageMod += 0.1f;
                     color = Color.white;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Steriods", "Raises mechanics damage by 10%", color);
                     break;
                 case 10:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryCarrySpeedMod += 0.2f;
                     color = Color.green;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Creatine", "While carrying a battery mechanic is 20% faster", color);
                     break;
                 case 11:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryThrowingDistanceAddMod += 0.3f;
                     color = Color.green;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Thor", "Allows the mechanic to throw batteries 30% farther", color);
                     break;
                 case 12:
                     PowerUpVars.GetComponent<PowerUpVariables>().SubTotalDamageTakenMod += 0.1f;
                     color = Color.green;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Titanium Gears", "Decreases damage taken by 10%", color);
                     break;
                 case 13:
                     PowerUpVars.GetComponent<PowerUpVariables>().ClockPowerMod += 0.3f;
                     color = Color.green;
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Unlimited Unlimted Power", "Increase clock power by 30%", color);
                     break;
                 case 14:
                     PowerUpVars.GetComponent<PowerUpVariables>().FullyRechargeChanceMod += 2;
                     color = new Color(128, 0, 128);
+                    PUI.DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "Cosmic Rays", "There is an extremley low chance every second to fully reacharge the clock", color);
                     break;
             }
-            GameObject.Find("PowerUpInvintory").GetComponent<PowerUpUi>().PickedUpPowerUp(GetComponent<SpriteRenderer>().sprite, color);
-            GameObject.Find("PowerUpInvintory").GetComponent<PowerUpUi>().DoPowerUpPopUp(GetComponent<SpriteRenderer>().sprite, "meth", "I love meth", color);
+            GameObject.Find("PowerUpInvintory").GetComponent<PowerUpUi>().PickedUpPowerUp(GetComponent<SpriteRenderer>().sprite, color);   
             Destroy(gameObject);
         }
     }
