fd5cd7e6652fb326a8b09aca824254ce05793f0f
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-28 10:37:58 -0700

    AddedPause Menu (Buttons Dont Function)

--- game/Assets/Scripts/Pause.cs
+++ game/Assets/Scripts/Pause.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     bool pause = false;
+    public GameObject PauseMenu;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (pause == false)
             {
+                PauseMenu.SetActive(true);
                 pause = true;
                 Time.timeScale = 0;
             } else {
                 pause = false;
                 Time.timeScale = 1;
+                PauseMenu.SetActive(false);
             }
         }
     }
