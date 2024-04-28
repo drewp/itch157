502498481aeb74228427bc4ecc8f390f0fef8c64
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-28 14:02:52 -0700

    Achivment Framework

---
+++ game/Assets/Achivement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achivment : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    void CallAchivement(string Name, string Description, Sprite Picture)
+    {
+
+    }
+}
