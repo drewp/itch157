11fa1f6626cc52d8bf3bc705206b4709c27b14fb
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-28 11:43:14 -0700

    fixed this

--- game/Assets/Scripts/PowerUpManager.cs
+++ game/Assets/Scripts/PowerUpManager.cs
@@ -27,8 +27,9 @@ public class PowerUpManager : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
+            Color color = new Color();
             PowerUpVars = GameObject.Find("PowerUpObject");
             PowerPickupSound.GetComponent<AudioSource>().Play();
             switch (Type)
@@ -36,48 +37,62 @@ public class PowerUpManager : MonoBehaviour
                 case 1:
                     //Fast Feet
                     PowerUpVars.GetComponent<PowerUpVariables>().MovementSpeedMod += 0.07f;
+                    color = Color.white;
                     break;
                 case 2:
                     PowerUpVars.GetComponent<PowerUpVariables>().ClockSpeedMod += 0.10f;
+                    color = Color.white;
                     break;
                 case 3:
                     PowerUpVars.GetComponent<PowerUpVariables>().EnergyDrainMod += 0.10f;
+                    color = Color.white;
                     break;
                 case 4:
                     PowerUpVars.GetComponent<PowerUpVariables>().ClockPowerAddMod += 0.10f;
+                    color = Color.white;
                     break;
                 case 5:
                     PowerUpVars.GetComponent<PowerUpVariables>().AttackSpeedMod += 0.10f;
+                    color = Color.white;
                     break;
                 case 6:
                     PowerUpVars.GetComponent<PowerUpVariables>().HealthAddMod += 0.10f;
+                    color = Color.white;
                     break;                
                 case 7:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryDropChanceMod += 20f;
+                    color = Color.white;
                     break;
                 case 8:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryRechargeMod += 0.2f;
+                    color = Color.white;
                     break;
                 case 9:
                     PowerUpVars.GetComponent<PowerUpVariables>().DamageMod += 0.1f;
+                    color = Color.white;
                     break;
                 case 10:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryCarrySpeedMod += 0.2f;
+                    color = Color.green;
                     break;
                 case 11:
                     PowerUpVars.GetComponent<PowerUpVariables>().BatteryThrowingDistanceAddMod += 0.3f;
+                    color = Color.green;
                     break;
                 case 12:
                     PowerUpVars.GetComponent<PowerUpVariables>().SubTotalDamageTakenMod += 0.1f;
+                    color = Color.green;
                     break;
                 case 13:
                     PowerUpVars.GetComponent<PowerUpVariables>().ClockPowerMod += 0.3f;
+                    color = Color.green;
                     break;
                 case 14:
                     PowerUpVars.GetComponent<PowerUpVariables>().FullyRechargeChanceMod += 2;
+                    color = new Color(128, 0, 128);
                     break;
             }
-            GameObject.Find("PowerUpInvintory").GetComponent<PowerUpUi>().PickedUpPowerUp(GetComponent<SpriteRenderer>().sprite);
+            GameObject.Find("PowerUpInvintory").GetComponent<PowerUpUi>().PickedUpPowerUp(GetComponent<SpriteRenderer>().sprite, color);
             Destroy(gameObject);
         }
     }
