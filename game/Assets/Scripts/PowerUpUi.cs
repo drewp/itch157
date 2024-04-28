003c3da1f5219295ebc7d625a4a20ae6f50fea77
PopcornCaleb <calebcukierman@gmail.com>
2024-04-28 01:42:03 -0700

    Power Up sprite Ui finished

--- game/Assets/Scripts/PowerUpUi.cs
+++ game/Assets/Scripts/PowerUpUi.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
+
 
 public class PowerUpUi : MonoBehaviour
 {
     private List<Sprite> PowerUpSprites;
     private List<GameObject> PowerUpHolders = new List<GameObject>();
+    public Sprite testSprite;
     void Start()
     {
         for (int i = 0; i < gameObject.transform.childCount; i++)
@@ -16,12 +20,23 @@ public class PowerUpUi : MonoBehaviour
         {
             PowerUpHolders[i].SetActive(false);
         }
+        PickedUpPowerUp(testSprite);
             
     }
     public void PickedUpPowerUp(Sprite sprite)
     {
         int index = PowerUpSprites.IndexOf(sprite);
-        Debug.Log(index);
-        
+        if (index!=-1)
+        {
+            PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = (Convert.ToInt32(PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text)+1 ).ToString();
+        }
+        else
+        {
+            PowerUpSprites.Add(sprite);
+            PowerUpHolders[index].GetComponent<SpriteRenderer>().sprite = sprite;
+            PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = (Convert.ToInt32(PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text) + 1).ToString();
+
+        }
+
     } 
 }
