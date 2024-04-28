ed11610c68efccf8957ff80d29ca12a1ef13b0a0
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-27 19:22:35 -0700

    Ari For You

--- game/Assets/Player_movement.cs
+++ game/Assets/Player_movement.cs
@@ -10,9 +10,24 @@ public class Player_movement : MonoBehaviour
     [SerializeField]
     private float runSpeed = 10.0f;
     public GameObject PowerUpScript;
-    
+
+    public AudioSource Step;
+
+    bool walking = false;
+
+    public
+
     void FixedUpdate()
     {
         rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * runSpeed;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) && walking == false)
+        {
+            Step.Play();
+        } else
+        {
+            Step.Stop();
+            walking = false;
+        }
     }
 }
