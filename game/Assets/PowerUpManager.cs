56dea3e88d7b8a28f3225150df5e70c9711c945d
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-28 09:11:20 -0700

    More Powerups

--- game/Assets/PowerUpManager.cs
+++ game/Assets/PowerUpManager.cs
@@ -53,7 +53,10 @@ public class PowerUpManager : MonoBehaviour
                     PowerUpVars.GetComponent<PowerUpVariables>().HealthAddMod += 0.10f;
                     break;                
                 case 7:
-                    PowerUpVars.GetComponent<PowerUpVariables>().BatteryDropChanceMod += 200f;
+                    PowerUpVars.GetComponent<PowerUpVariables>().BatteryDropChanceMod += 20f;
+                    break;
+                case 8:
+                    PowerUpVars.GetComponent<PowerUpVariables>().BatteryRechargeMod += 100.2f;
                     break;
             }
             GameObject.Find("PowerUpInvintory").GetComponent<PowerUpUi>().PickedUpPowerUp(GetComponent<SpriteRenderer>().sprite);
--- game/Assets/Scripts/BatteryScriptable.cs
+++ game/Assets/Scripts/BatteryScriptable.cs
@@ -8,9 +8,14 @@ public class BatteryScriptable : MonoBehaviour
     public float TimeToRefill;
     public float Weight;
 
+    public GameObject PowerUpVars;
+
     void Start()
     {
-        PowerRefilled = Random.Range(500, 2000);
+        PowerUpVars = GameObject.Find("PowerUpObject");
+        float min = 500 * PowerUpVars.GetComponent<PowerUpVariables>().BatteryRechargeMod;
+        float max = 2000 * PowerUpVars.GetComponent<PowerUpVariables>().BatteryRechargeMod;
+        PowerRefilled = Random.Range((int)min, (int)max);
     }
     void Update()
     {
