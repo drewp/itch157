ad3fcbdd378f214dcd82901d16990ab1bcdd6a3f
PopcornCaleb <calebcukierman@gmail.com>
2024-04-28 02:20:36 -0700

    Robot hurt

--- game/Assets/Scripts/PowerUpUi.cs
+++ game/Assets/Scripts/PowerUpUi.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using System;
 
 
@@ -9,7 +10,7 @@ public class PowerUpUi : MonoBehaviour
 {
     private List<Sprite> PowerUpSprites;
     private List<GameObject> PowerUpHolders = new List<GameObject>();
-    public Sprite testSprite;
+    private int PlaceInHolderOn = 0;
     void Start()
     {
         for (int i = 0; i < gameObject.transform.childCount; i++)
@@ -20,22 +21,22 @@ public class PowerUpUi : MonoBehaviour
         {
             PowerUpHolders[i].SetActive(false);
         }
-        PickedUpPowerUp(testSprite);
-            
+
     }
     public void PickedUpPowerUp(Sprite sprite)
     {
         int index = PowerUpSprites.IndexOf(sprite);
         if (index!=-1)
         {
-            PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = (Convert.ToInt32(PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text)+1 ).ToString();
+            PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = (Convert.ToInt32(PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text)+1 ).ToString();
         }
         else
         {
             PowerUpSprites.Add(sprite);
-            PowerUpHolders[index].GetComponent<SpriteRenderer>().sprite = sprite;
-            PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = (Convert.ToInt32(PowerUpHolders[index].transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text) + 1).ToString();
-
+            PowerUpHolders[PlaceInHolderOn].GetComponent<Image>().sprite = sprite;
+            PowerUpHolders[PlaceInHolderOn].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = "1";
+            PowerUpHolders[PlaceInHolderOn].SetActive(true);
+            PlaceInHolderOn++;
         }
 
     } 
