bae7194436677450837789aa9f24e0d0ea623ee4
PopcornCaleb <calebcukierman@gmail.com>
2024-04-28 15:04:39 -0700

    Added name to pop up

--- game/Assets/Scripts/PowerUpUi.cs
+++ game/Assets/Scripts/PowerUpUi.cs
@@ -14,14 +14,17 @@ public class PowerUpUi : MonoBehaviour
     private GameObject PowerUpImage;
     private GameObject PowerUpInfo;
     private GameObject PowerUPPopUp;
+    private GameObject PowerUpName;
     private bool DoingPopUp = false;
     private List<string> PopUpInfos = new List<string>();
     private List<Sprite> PopUpSprites = new List<Sprite>();
     private List<Color> PopUpColors = new List<Color>();
+    private List<string> PopUpNames = new List<string>();
     void Start()
     {
         PowerUpImage = transform.Find("PowerUpPopUp").Find("PowerUpImage").gameObject;
         PowerUpInfo = transform.Find("PowerUpPopUp").Find("PowerUpInfo").gameObject;
+        PowerUpName = transform.Find("PowerUpPopUp").Find("PowerUpName").gameObject;
         PowerUPPopUp = transform.Find("PowerUpPopUp").gameObject;
 
         for (int i = 0; i < gameObject.transform.childCount; i++)
@@ -61,20 +64,23 @@ public class PowerUpUi : MonoBehaviour
             PowerUpImage.GetComponent<Image>().sprite = PopUpSprites[0];
             PowerUpInfo.GetComponent<TextMeshProUGUI>().text = PopUpInfos[0];
             PowerUpInfo.GetComponent<TextMeshProUGUI>().color = PopUpColors[0];
+            PowerUpName.GetComponent<TextMeshProUGUI>().text = PopUpNames[0];
             StartCoroutine(PopUpPopOut());
             PopUpColors.RemoveAt(0);
             PopUpInfos.RemoveAt(0);
             PopUpSprites.RemoveAt(0);
+            PopUpNames.RemoveAt(0);
         }
     }
 
 
 
-    public void DoPowerUpPopUp(Sprite Image, string description, Color TextColor)
+    public void DoPowerUpPopUp(Sprite Image,string name ,string description, Color TextColor)
     {
         PopUpSprites.Add(Image);
         PopUpInfos.Add(description); 
         PopUpColors.Add(TextColor);
+        PopUpNames.Add(name);
 
     }
 
