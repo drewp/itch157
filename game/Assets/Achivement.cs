7ab01edef8b1e136d0f8c42d1fc43f2f2dfcb2e5
galaxyraider9wastaken <owendahms@gmail.com>
2024-04-28 19:32:46 -0700

    avhity

--- game/Assets/Achivement.cs
+++ game/Assets/Achivement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Achivment : MonoBehaviour
 {
+    List<SpriteRenderer> AchPics;
+
     void Start()
     {
         
@@ -13,8 +15,57 @@ public class Achivment : MonoBehaviour
         
     }
 
-    void CallAchivement(string Name, string Description, Sprite Picture)
+    void CallAchievement(int type, Sprite Picture)
     {
+        switch (type){
+            case 1:
 
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+            case 12:
+
+                break;
+            case 13:
+
+                break;
+            case 14:
+
+                break;
+            case 15:
+
+                break;
+            case 16:
+
+                break;
+        }
     }
 }
