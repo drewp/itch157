7a253b5c6383bc2e60d2d0a75d41f7ab2a0ac1b9
arrowira <41136800+arrowira@users.noreply.github.com>
2024-04-28 20:40:46 -0700

    powerup spawning (didn't work

--- game/Assets/Scripts/PowerupSpawning.cs
+++ game/Assets/Scripts/PowerupSpawning.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PowerupSpawning : MonoBehaviour
 {
+    
+    [SerializeField]
+    private float width = 40f;
+    [SerializeField]
+    private float height = 40f;
+    [SerializeField]
+    private int min = 3;
+    [SerializeField]
+    private int max = 15;
+    GameObject PowerUpVars;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PowerUpVars = GameObject.Find("PowerUpObject");
+        int amount = Random.Range(min, max);
+        for (int i = 0; i < amount; i++)
+        {
+            float xcord = Random.Range(-width, width);
+            float ycord = Random.Range(-height, height);
+            GameObject PPowerup = PowerUpVars.GetComponent<PowerUpVariables>().OnDrop();
+            if (PPowerup != null && Random.Range(1, 7) == Random.Range(1, 10))
+            {
+                GameObject Temp = Instantiate(PPowerup);
+                Temp.transform.position = new Vector2(xcord, ycord);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
